b62f8ee29e52f9e0feb6af5ecd53800ccfefdca7
Arcath <adam@arcath.net>
2012-11-13 09:22:27 +0000

    SSH Key Installation

--- lib/quick/ssh.rb
+++ lib/quick/ssh.rb
@@ -11,9 +11,20 @@ module Quick
     
     def process_command
       if @config[@name.to_sym]
-        ssh_command = "ssh #{@config[@name.to_sym][:hostname]}"
-        ssh_command = add_options(ssh_command)
-        system(ssh_command)
+        if @arguments == []
+          ssh_command = "ssh #{@config[@name.to_sym][:hostname]}"
+          ssh_command = add_options(ssh_command)
+          system(ssh_command)
+        elsif @arguments.first == "-i"
+          puts "Installing SSH Key"
+          puts "When prompted give the remote users password"
+          host_user = (@config[@name.to_sym][:username] || ENV['USER'])
+          system("mkdir -p data/#{@name}")
+          system("scp -p #{host_user}@#{@config[@name.to_sym][:hostname]}:~/.ssh/authorized_keys2 data/#{@name}/ssh_keys")
+          system("cat ~/.ssh/id_rsa.pub >> data/#{@name}/ssh_keys")
+          system("scp -p data/#{@name}/ssh_keys #{host_user}@#{@config[@name.to_sym][:hostname]}:~/.ssh/authorized_keys2")
+          puts "Key installed! try `quick ssh #{@name}`"
+        end
       elsif @name == "-l"
         @config.each do |key, value|
           puts key.to_s
@@ -23,11 +34,7 @@ module Quick
         end
       else
         if @arguments.first == "-a"
-          @config[@name.to_sym] = {}
-          @config[@name.to_sym][:username] = fetch_argument("-u") if @arguments.index("-u")
-          @config[@name.to_sym][:hostname] = fetch_argument("-h") if @arguments.index("-h")
-          @config[@name.to_sym][:port] = fetch_argument("-p") if @arguments.index("-p")
-          Quick::Config.dump('ssh', @config)
+          add_host
         else
           puts "Host not found"
         end
@@ -42,6 +49,14 @@ module Quick
       cmd += " -l #{@config[@name.to_sym][:username]}" if @config[@name.to_sym][:username]
       cmd += " -p #{@config[@name.to_sym][:port]}" if @config[@name.to_sym][:port]
       return cmd
+    end
+    
+    def add_host
+      @config[@name.to_sym] = {}
+      @config[@name.to_sym][:username] = fetch_argument("-u") if @arguments.index("-u")
+      @config[@name.to_sym][:hostname] = fetch_argument("-h") if @arguments.index("-h")
+      @config[@name.to_sym][:port] = fetch_argument("-p") if @arguments.index("-p")
+      Quick::Config.dump('ssh', @config)
     end
 	end
 end
